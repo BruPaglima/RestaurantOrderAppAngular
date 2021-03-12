43afd8cb8de76511e8ca3ca22383959c686a0701
bruno.lima <bplcorporativo@outlook.com>
2021-03-12 11:49:38 -0300

    Angular project commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
